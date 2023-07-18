b71e14a170d7c980e4826ed108886926171be286
Villarroya-Palau Arthur <arthur.villarroya-palau@etu.umontpellier.fr>
2023-07-18 18:42:20 +0200

    opencv Arthur organisation

--- Reconizer/main.cpp
+++ Reconizer/main.cpp
@@ -6,9 +6,10 @@ using namespace cv;
 
 int main()
 {
-    cv::Mat img = cv::imread("D:/Documents/Projets Dev/ReconIzer/Data/abstract-rainbow-colors-background.jpg");
+    //cv::Mat img = cv::imread("D:/Documents/Projets Dev/ReconIzer/Data/abstract-rainbow-colors-background.jpg");
+    cv::Mat img2 = cv::imread("C:/Users/arthu/Pictures/lol.png");
     namedWindow("First OpenCV Application", WINDOW_AUTOSIZE);
-    cv::imshow("First OpenCV Application", img);
+    cv::imshow("First OpenCV Application", img2);
     cv::moveWindow("First OpenCV Application", 0, 45);
     cv::waitKey(0);
     cv::destroyAllWindows();
