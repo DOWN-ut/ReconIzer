0b5db7447eb377b6091f86385799c5823b173497
Yahnis Saint-Val <yahnis.saint-val@etu.umontpellier.fr>
2023-07-28 21:57:19 +0200

    TrackThmnail ideo metjhod

--- Reconizer/VideoOperations.cpp
+++ Reconizer/VideoOperations.cpp
@@ -7,3 +7,16 @@ void VideoOperations::UnitedColor(int R, int G, int B, Video* video)
 		video->GetFrame(i)->SetImage(ImageOpperations::UnitedColor(R, G, B, video->GetFrame(i)->Image()));
 	}
 }
+
+void VideoOperations::TrackThumbnail(Video* video, Thumbnail* thumbnail)
+{
+	for (int i = 0; i < video->Length(); i++)
+	{
+		Frame* frame = video->GetFrame(i);
+
+		cv::Vec3f detectedPosition = ImageOpperations::ThumbnailFrameTracker(thumbnail, frame->Image());
+
+		cout << "Detected at : (" << detectedPosition[0] << "," << detectedPosition[1] << ")"
+			<< " with a PSNR of " << detectedPosition[2] << endl;
+	}
+}
--- Reconizer/VideoOperations.h
+++ Reconizer/VideoOperations.h
@@ -1,10 +1,15 @@
 #pragma once
+#include <iostream>
 #include "Video.h"
 #include "ImageOpperations.h"
+
+using namespace std;
 
 class VideoOperations
 {
 public:
 	static void UnitedColor(int R, int G, int B, Video* video);
+
+	static void TrackThumbnail(Video* video, Thumbnail* thumbnail);
 };
 
