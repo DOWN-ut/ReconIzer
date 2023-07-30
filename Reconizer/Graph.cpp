7225a80e8153ac3593e6e68472ddca8078deaa9b
Yahnis Saint-Val <yahnis.saint-val@etu.umontpellier.fr>
2023-07-30 17:48:01 +0200

    correction

--- Reconizer/Graph.cpp
+++ Reconizer/Graph.cpp
@@ -4,6 +4,14 @@ void Graph::Process()
 {
 }
 
+bool Graph::Compare(Graph* over)
+{
+	for (int i = 0; i < size; i++)
+	{
+		bool maxok = MaxAt(i) <= over->MaxAt(i);
+	}
+}
+
 Graph::Graph(cv::Mat* data)
 {
 
--- Reconizer/Graph.h
+++ Reconizer/Graph.h
@@ -4,7 +4,7 @@
 class Graph
 {
 protected:
-	float* max;
+	float* maxs;
 	float* mins;
 	float* averages;
 
@@ -13,9 +13,14 @@ protected:
 
 	void Process();
 
+	bool Compare(Graph* over);
+
 public:
 	Graph() {  };
 	Graph(cv::Mat* data);
 
+	float MaxAt(int i) { return maxs[i]; }
+	float MinAt(int i) { return mins[i]; }
+	float AverageAt(int i) { return averages[i]; }
 };
 
