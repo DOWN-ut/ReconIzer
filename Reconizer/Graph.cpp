8f27633672d8a69b5b499c160d9f640615db1ac1
Yahnis Saint-Val <yahnis.saint-val@etu.umontpellier.fr>
2023-07-30 17:54:21 +0200

    float ot vec3f

--- Reconizer/Graph.cpp
+++ Reconizer/Graph.cpp
@@ -4,12 +4,18 @@ void Graph::Process()
 {
 }
 
-bool Graph::Compare(Graph* over)
+bool Graph::Compare(Graph* over, int position)
 {
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < size && position + i < over->size; i++)
 	{
-		bool maxok = MaxAt(i) <= over->MaxAt(i);
+		if (!(
+			MaxAt(i) <= over->MaxAt(position + i) &&
+			MinAt(i) >= over->MinAt(position + i)))
+		{
+			return false;
+		}
 	}
+	return true;
 }
 
 Graph::Graph(cv::Mat* data)
--- Reconizer/Graph.h
+++ Reconizer/Graph.h
@@ -4,23 +4,23 @@
 class Graph
 {
 protected:
-	float* maxs;
-	float* mins;
-	float* averages;
+	cv::Vec3f* maxs;
+	cv::Vec3f* mins;
+	cv::Vec3f* averages;
 
 	int size;
 	cv::Mat* data;
 
 	void Process();
 
-	bool Compare(Graph* over);
+	bool Compare(Graph* over, int position);
 
 public:
 	Graph() {  };
 	Graph(cv::Mat* data);
 
-	float MaxAt(int i) { return maxs[i]; }
-	float MinAt(int i) { return mins[i]; }
-	float AverageAt(int i) { return averages[i]; }
+	cv::Vec3f MaxAt(int i) { return maxs[i]; }
+	cv::Vec3f MinAt(int i) { return mins[i]; }
+	cv::Vec3f AverageAt(int i) { return averages[i]; }
 };
 
